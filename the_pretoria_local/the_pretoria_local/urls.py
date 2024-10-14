3ed94ef0ed1e9eeba1bfeb20040b28c9f47a6867
RyanLouwSAC <ryan.l@sactrucks.co.za>
2024-10-14 14:04:54 +0200

    updates

--- the_pretoria_local/the_pretoria_local/urls.py
+++ the_pretoria_local/the_pretoria_local/urls.py
@@ -4,10 +4,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('directory.urls')),  # Include the directory app URLs
+    path('googlebc4cc8e1dbd0d0cd.html', TemplateView.as_view(
+        template_name="directory/google_verification/googlebc4cc8e1dbd0d0cd.html",
+        content_type='text/html'
+    )),
 ]
 
 # Serve static files in development
